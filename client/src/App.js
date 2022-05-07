583d6b0adefbad911c35da402195959ae0b22562
Sam Senior <samuelsenior@developersinstitute.co.nz>
2022-05-07 16:38:17 +1200

    Added react app

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from "react";
+import "./App.css";
+
+import MintPage from "./components/MintPage";
+function App() {
+  return (
+    <Fragment>
+      <div className="container">
+        <MintPage />
+      </div>
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ client/src/components/MintPage.js
@@ -0,0 +1,11 @@
+import React, { Fragment, useEffect, useState } from "react";
+
+const MintPage = () => {
+  return (
+    <Fragment>
+      <h1>NFT minting site</h1>
+    </Fragment>
+  );
+};
+
+export default MintPage;
